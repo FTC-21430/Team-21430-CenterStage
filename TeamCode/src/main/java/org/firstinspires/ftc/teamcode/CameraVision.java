be6d564b32112a863c7f2d002f1c9f9809915622
FriendlyGuardian <purplepenguin998@gmail.com>
2023-11-26 19:04:30 -0700

    added Team Prop tensorflow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraVision.java
@@ -59,13 +59,14 @@ public double y;
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "model_20231014_120906.tflite";
+    private static final String TFOD_MODEL_ASSET = "Team_Props.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
     private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/model_20231014_120906.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
-            "white pixel",
+            "blue-prop",
+            "red-prop",
     };
 
     /**
